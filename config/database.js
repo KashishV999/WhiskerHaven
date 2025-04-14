05bcea2e3071cd8526877e8ab1af666372863eb2
Kashish Verma <kverma45@myseneca.ca>
2025-04-14 01:03:55 -0400

    Test

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose
+     await mongoose
     .connect("mongodb://127.0.0.1:27017/WhiskersWay")
     .then(() => {
       console.log("MongoDB connected successfully");
